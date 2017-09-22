2caac7e61dec7b0b2e60e1599707b17369e4ae89
Sergio Ángel Verbo <seranfuen@gmail.com>
2017-09-22 12:02:55 +0200

    First commit as scaffolded by the project wizard in visual studio

---
+++ DreamDiaryMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DreamDiaryMvc.Startup))]
+namespace DreamDiaryMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
